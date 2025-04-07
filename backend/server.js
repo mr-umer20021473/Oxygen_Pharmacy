889ce1d478a70be20636e18282d2d40ba42d109f
mr-umer20021473 <umeramanullah123@gmail.com>
2025-04-07 17:25:24 +0100

    database connections added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 dotenv.config();
+
+const port = process.env.PORT
+
+connectDB()
 
 
 const app = express()
 
 
-app.listen(5000,()=> console.log("Server Running on port 5000"))
+app.listen(port,()=> console.log(`Server Running on port ${port}`))
